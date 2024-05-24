526f2549bbb3d74b5ef615e06f71b784e12c48d2
Lucas Fogliarini <lucasfogliarini@gmail.com>
2024-05-24 13:39:32 -0300

    Add Place in Location

---
+++ Bora.Repository.EFCore/Migrations/20240524163554_AddPlaceInLocation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bora.Repository.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlaceInLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "Location",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "Location");
+        }
+    }
+}
--- Bora/Entities/Location.cs
+++ Bora/Entities/Location.cs
@@ -3,6 +3,7 @@
 	public class Location : Entity
 	{
         public string Name { get; set; }
+        public string? Place { get; set; }
         public int AccountId { get; set; }
         public Account Account { get; set; }
     }
