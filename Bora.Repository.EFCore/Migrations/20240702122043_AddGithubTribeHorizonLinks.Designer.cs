76228e6ade68280209241270cb8070472c7cadb6
Lucas Fogliarini <lucasfogliarini@gmail.com>
2024-07-02 09:21:05 -0300

    AddGithubTribeHorizonLinks

--- Bora.Repository.EFCore/Migrations/20240702122043_AddGithubTribeHorizonLinks.Designer.cs
+++ Bora.Repository.EFCore/Migrations/20240702122043_AddGithubTribeHorizonLinks.Designer.cs
@@ -4,6 +4,7 @@ using Bora.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bora.Repository.EFCore.Migrations
 {
     [DbContext(typeof(BoraDbContext))]
-    partial class BoraDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240702122043_AddGithubTribeHorizonLinks")]
+    partial class AddGithubTribeHorizonLinks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Bora.Repository.EFCore/Migrations/20240702122043_AddGithubTribeHorizonLinks.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bora.Repository.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGithubTribeHorizonLinks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Github",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HorizonMeta",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tribe",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Github",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "HorizonMeta",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "Tribe",
+                table: "Account");
+        }
+    }
+}
